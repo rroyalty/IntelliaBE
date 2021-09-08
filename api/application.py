97a4fdedce6905d1293515ed1635222d4e2bcbc7
rroyalty <rroyalty@gmail.com>
2021-09-08 18:53:36 -0400

    Tinkering with SQLAlchemy

--- api/application.py
+++ api/application.py
@@ -1,6 +1,25 @@
-from flask import flask
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
-@app.route('/'):
-    def index():
-        return 'Hello!'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+db = SQLAlchemy(app)
+
+class Lesson(db.Model):
+    id: db.Column(db.Integer, primary_key=True)
+
+
+# Home Route
+@app.route('/')
+def index():
+    return 'Hello!'
+
+# All lesson plans
+@app.route('/plans')
+def get_plans():
+    return{"Plans": "Plan Data"}
+
+# Lesson plan by ID
+@app.route('/plans/:id')
+def get_plans():
+    return{"Plans": "Plan Data"}
