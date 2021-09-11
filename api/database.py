779ef99fb4d4978cf589de172a4585ef2df01a56
rroyalty <rroyalty@gmail.com>
2021-09-11 08:56:54 -0400

    Final update on this repo. Recreating repo for clean slate.

--- api/database.py
+++ api/database.py
@@ -4,7 +4,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = 'mysql+mysqlconnector://root:youcannotpassword@localhost/LessonPlans'
+
+SQLALCHEMY_DATABASE_URL = 'mysql+mysqlconnector://hutmjiwus27e4j9t:xd8010ms79n0w4wa@s29oj5odr85rij2o.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/f3373jcbm64lndzf'
 
 # os.getenv("DB_CONN")
 
@@ -12,3 +13,4 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
