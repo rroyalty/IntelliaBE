f0632b352108e7b40952dd2671be3451beff036e
rroyalty <rroyalty@gmail.com>
2021-09-17 09:49:49 -0400

    Successfully passing db data to template.

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI, Request
-from pydantic.networks import HttpUrl
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from .routes.lessons import *
 from fastapi.middleware.cors import CORSMiddleware
 import requests
@@ -15,9 +17,14 @@ app.add_middleware(
     allow_credentials=True,
 )
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="static/templates")
+
 app.include_router(router)
 
 @app.get('/list')
 def show_list(request: Request):
-    data = requests.get(request.url_for('read_lessons'))
+    data = requests.get(request.url_for('read_lessons')).json()
     print(data)
+    return templates.TemplateResponse("list.html", {"request": request, "data": data})
+    
